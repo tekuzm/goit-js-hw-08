556b0caba9edbb3fa7a77cbd2d5798afec33c985
TetianaKuzma <tetiana.b97@gmail.com>
2022-11-19 14:23:48 +0200

    vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const playerIframe = document.querySelector('#vimeo-player');
+const player = new Player(playerIframe);
+
+const onPlay = ({ sec }) => {
+  localStorage.setItem('videoplayer-current-time', sec);
+};
+
+player.on('timeuptodate', throttle(onPlay, 1000));
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
