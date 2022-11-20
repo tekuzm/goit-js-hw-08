044773bb675f4da1759631fafc830a4deab19fa5
TetianaKuzma <tetiana.b97@gmail.com>
2022-11-20 13:04:11 +0200

    add feedback-form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,9 +5,32 @@ const email = document.querySelector('input[name="email"]');
 const message = document.querySelector('textarea[name="message"]');
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 
-const saveToLocalStorage = () => {
-  localStorage.setItem(LOCALSTORAGE_KEY, email.value);
-  debugger;
+const onInput = () => {
+  const objectForSave = { email: email.value, message: message.value };
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(objectForSave));
 };
 
-feedbackForm.addEventListener('submit', saveToLocalStorage);
+const onSubmit = e => {
+  e.preventDefault();
+  console.log({ email: email.value, message: message.value });
+  feedbackForm.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+};
+
+feedbackForm.addEventListener('input', throttle(onInput, 500));
+feedbackForm.addEventListener('submit', onSubmit);
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+const dataSaved = load(LOCALSTORAGE_KEY);
+if (dataSaved) {
+  email.value = dataSaved.email;
+  message.value = dataSaved.message;
+}
